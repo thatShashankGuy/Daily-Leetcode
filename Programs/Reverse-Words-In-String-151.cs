3a0dc40de5e766c608fdca739a82e8ef6af8c7bc
thatshashankguy <shahankforworkshekhar@gmail.com>
2024-09-28 16:48:09 +0530

    python solution

--- Programs/Reverse-Words-In-String-151.cs
+++
@@ -1,7 +0,0 @@
-public class Solution {
-    public string ReverseWords(string s) {
-        string[] words = s.Split(new char[]{' '}, StringSplitOptions.RemoveEmptyEntries);
-        Array.Reverse(words);
-        return string.Join(' ', words);
-    }
-}
